5a35c22ca27853760761a1f24da4ffc9624bd564
SirMarkus <marcosbarreroherrero@gmail.com>
2024-12-27 18:13:07 +0100

    animate exit and clip path

--- src/components/todo/todo.tsx
+++ src/components/todo/todo.tsx
@@ -1,42 +1,57 @@
+import { useState } from "react"
 import type { TodoId, Todo as TodoInterface } from "../../types.d.ts"
 import "./todo.css"
-import { motion } from "motion/react"
+import { motion, AnimatePresence } from "motion/react"
 interface Props extends TodoInterface {
   removeFunction: ({ id }: { id: TodoId }) => void
 }
 
 export function Todo({ id, title, description, tags, removeFunction }: Props) {
+  const [isActive, setActive] = useState(true)
+
   const onClickRemoveTodo = () => {
+    setActive(!isActive)
+  }
+
+  const onExitComplete = () => {
     removeFunction({ id })
   }
 
   return (
-    <motion.article
-      className="todo"
-      layout
-      initial={{ opacity: 0 }}
-      animate={{
-        opacity: 1,
-        transition: { duration: 1, delay: 0.525 },
-      }}
-      transition={{ duration: 0.5 }}
-    >
-      <h2>{title}</h2>
-      <p>{description}</p>
-      {tags && (
-        <ul>
-          {tags.map((tagName, index) => (
-            <li key={`${tagName}-${index}`}>{tagName}</li>
-          ))}
-        </ul>
+    <AnimatePresence onExitComplete={onExitComplete}>
+      {isActive && (
+        <motion.article
+          className="todo"
+          layout
+          initial={{
+            clipPath: "inset(0 100% 0 0)",
+          }}
+          animate={{
+            transition: { duration: 1, delay: 0.525 },
+            clipPath: "inset(0 0 0 0)",
+          }}
+          exit={{
+            clipPath: "inset(0 100% 0 0)",
+          }}
+        >
+          <h2>{title}</h2>
+          <p>{description}</p>
+          {tags && (
+            <ul>
+              {tags.map((tagName, index) => (
+                <li key={`${tagName}-${index}`}>{tagName}</li>
+              ))}
+            </ul>
+          )}
+          <button
+            type="button"
+            className="remove-button"
+            onClick={onClickRemoveTodo}
+          >
+            Eliminar
+          </button>
+        </motion.article>
       )}
-      <button
-        type="button"
-        className="remove-button"
-        onClick={onClickRemoveTodo}
-      >
-        Eliminar
-      </button>
-    </motion.article>
+    </AnimatePresence>
   )
 }
